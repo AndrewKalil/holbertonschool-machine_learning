dbcbcaf5dfeb9db37c315fbe74360fbfb4375425
AndrewKalil <1541@holbertonschool.com>
2020-12-09 16:10:09 -0500

    checking if algorithms are correct

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Our life is the sum total of all the decisions we make 
+    every day, and those decisions are determined by our priorities  """
+
+def summation_i_squared(n):
+    """ making a summation function """
+
+    if type(n) is not int or n < 1:
+        return None
+    if n == 1: 
+        return 1
+    else:
+        return int((n*((n+1)*(2*n+1)))/ 6)
