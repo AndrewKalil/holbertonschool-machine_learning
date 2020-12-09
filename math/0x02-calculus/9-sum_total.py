e44268cdf036586117cb634e847bc09146ba6354
AndrewKalil <1541@holbertonschool.com>
2020-12-09 16:16:00 -0500

    autopeping 9 and 10

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,12 +2,13 @@
 """ Our life is the sum total of all the decisions we make 
     every day, and those decisions are determined by our priorities  """
 
+
 def summation_i_squared(n):
     """ making a summation function """
 
     if type(n) is not int or n < 1:
         return None
-    if n == 1: 
+    if n == 1:
         return 1
     else:
-        return int((n*((n+1)*(2*n+1)))/ 6)
+        return int((n*((n+1)*(2*n+1))) / 6)
