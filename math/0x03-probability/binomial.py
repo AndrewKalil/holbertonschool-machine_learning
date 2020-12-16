ca1114ef89ab550885a16cd91ffb2d0a7e80a91f
AndrewKalil <1541@holbertonschool.com>
2020-12-16 18:02:03 -0500

    forgot initial requirements

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -76,8 +76,12 @@ class Binomial:
         Args:
             k (int): number of “successes”
         """
-        cdf = 0
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
 
+        cdf = 0
         for i in range(k+1):
             cdf += self.pmf(i)
         return cdf
