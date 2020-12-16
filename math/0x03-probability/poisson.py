5abf2872ce0a7d3da5f1e683a64faa20ba402be3
AndrewKalil <1541@holbertonschool.com>
2020-12-16 11:02:02 -0500

    adding done task

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+""" Poisson Distribution """
+
+def factorial(n):
+    """calculates the factrial of a number
+
+    Args:
+        n (int): number to find the factorial for
+
+    Returns:
+        int: factorial of n
+    """
+    if n == 0:
+        return 1
+
+    fact = 1
+
+    for i in range(1,n+1):
+        fact = fact * i
+    return fact
+
+class Poisson:
+    """ Represents the Posisson distribution """
+    def __init__(self, data=None, lambtha=1):
+        """Instantiation method
+
+        Args:
+            data (list): data to be used to estimate the distribution.
+                Defaults to None.
+            lambtha (int, optional): expected number of occurences in a given time frame.
+                Defaults to 1.
+        """
+        self.e = 2.7182818285
+
+        if data is None:
+            self.lambtha = float(lambtha)
+        elif lambtha <= 0:
+            raise ValueError('lambtha must be a positive value')
+        elif type(data) is not list:
+            raise TypeError('data must be a list')
+        elif len(data) <= 2:
+            raise ValueError('data must contain multiple values')
+        else:
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """probability mass function
+
+        Args:
+            k (int): number of successes
+        """
+        if k < 0:
+            return 0
+        if type(k) is not int:
+            k = int(k)
+        summation = ((self.e**-self.lambtha) * (self.lambtha**k)) / factorial(k)
+        return summation
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”
+
+        Args:
+            k (int):  the number of “successes”
+        """
+        if k < 0:
+            return 0
+        if type(k) is not int:
+            k = int(k)
+        summation = 0
+        for i in range(0, k+1):
+            summation = summation + self.pmf(i)
+        return summation
