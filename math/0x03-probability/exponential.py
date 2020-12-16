0ce7a5393e85e8dd4a75849e2654db3dca6ddffa
AndrewKalil <1541@holbertonschool.com>
2020-12-16 12:20:23 -0500

    adding exponential class

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python3
 """ Poisson Distribution """
 
+class Exponential:
+    """ that represents an exponential distribution
+    """
+    def __init__(self, data=None, lambtha=1.):
+        """Instantiation
+
+        Args:
+            data (list): data to be used to estimate the distribution. Defaults to None.
+            lambtha (float): expected number of occurences in a given time frame. Defaults to 1..
+        """
+        self.e = 2.7182818285
+        if data:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = 1 / float(sum(data) / len(data))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+
+    def pdf(self, x):
+        """Probability density function
+
+        Args:
+            x (int): time period
+        """
+        if x < 0:
+            return 0
+        return self.lambtha * self.e**(-self.lambtha*x)
+
+    def cdf(self, x):
+        """continuous density function
+
+        Args:
+            x ([type]): [description]
+        """
+        if x < 0:
+            return 0
+        return 1 - self.e**(-self.lambtha*x)
