36f30134bb8d96e708d895ea692249f70b2c76df
AndrewKalil <1541@holbertonschool.com>
2021-01-04 11:05:19 -0500

    adding if condition

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,6 +6,10 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """ Function for one hot encoding"""
 
+    if Y == None and classes < 1:
+        return None
+
     matrix = np.zeros((len(Y), classes))
     matrix[np.arange(len(Y)), Y] = 1
+
     return matrix.T
