9297c1cccdff1e9b24149d24b80fcdd59220ab57
AndrewKalil <1541@holbertonschool.com>
2021-01-04 12:00:32 -0500

    adding another test case

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,10 +6,10 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """ Function for one hot encoding"""
 
-    if Y is None or classes < 1:
+    if Y is None or classes < 1 or type(classes) != int:
         return None
 
-    matrix = np.zeros((classes, len(Y)))
+    matrix = np.zeros((len(Y), classes))
     matrix[np.arange(len(Y)), Y] = 1
 
     return matrix.T
