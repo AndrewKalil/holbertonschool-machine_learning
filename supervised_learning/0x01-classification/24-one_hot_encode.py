2bef7ea3754245e7a7088589981e291ced4fdab7
AndrewKalil <1541@holbertonschool.com>
2021-01-04 09:56:25 -0500

    fixing 24 a little

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,7 +5,11 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     emptyMatrix = np.zeros((len(Y), classes))
-    for i in range(classes):
-        emptyMatrix[i][Y[i]] = 1
 
-    return emptyMatrix.T
+    if Y is not None:
+        for i in range(classes):
+            emptyMatrix[i][Y[i]] = 1
+
+        return emptyMatrix.T
+    else:
+        return None
