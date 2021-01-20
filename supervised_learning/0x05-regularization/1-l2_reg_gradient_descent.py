1870902ad6c0266b5435b31089fcdf37781f2d40
AndrewKalil <1541@holbertonschool.com>
2021-01-20 13:59:52 -0500

    fixing task 2

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -23,16 +23,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     weights2 = weights.copy()
     m = Y.shape[1]
 
-    for i in reversed(range(L)):
-        n = i + 1
+    for net_ly in reversed(range(L)):
+
+        n = net_ly + 1
         if (n == L):
             dz = cache["A" + str(n)] - Y
-            dw = (np.matmul(cache["A" + str(i)], dz.T) / m).T
+            dw = (np.matmul(cache["A" + str(net_ly)], dz.T) / m).T
         else:
             dz1 = np.matmul(weights2["W" + str(n + 1)].T, current_dz)
             dz2 = 1 - cache["A" + str(n)]**2
             dz = dz1 * dz2
-            dw = np.matmul(dz, cache["A" + str(i)].T) / m
+            dw = np.matmul(dz, cache["A" + str(net_ly)].T) / m
 
         dw_reg = dw + (lambtha / m) * weights2["W" + str(n)]
         db = np.sum(dz, axis=1, keepdims=True) / m
