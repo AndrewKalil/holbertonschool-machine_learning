885397de0761af827de0329d817b719427261779
AndrewKalil <1541@holbertonschool.com>
2021-01-20 13:57:51 -0500

    fixing task 0

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -20,9 +20,10 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     λ = lambtha
     sum_weights = 0
 
-    for keys, values in weights.items():
-        if (keys == 'w'):
+    for keys in weights:
+        if (keys[0] == "W"):
+            values = weights[keys]
             sum_weights += np.linalg.norm(values)
 
-    cost_l2 = cost + (λ / 2 * m) * sum_weights
+    cost_l2 = cost + (λ / (2 * m)) * sum_weights
     return cost_l2
