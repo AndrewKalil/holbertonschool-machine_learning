7d6de9574a7bdb4a8a77986c7206b4a6ff8df8ab
AndrewKalil <1541@holbertonschool.com>
2021-01-20 15:47:52 -0500

    trying to switch up 3

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -4,19 +4,20 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """creates a tensorflow layer that includes L2 regularization
-
+    """Creates a tensorflow layer that includes L2 regularization.
     Args:
-        prev is a tensor containing the output of the previous layer
-        n is the number of nodes the new layer should contain
-        activation is the activation function that should be used on the layer
-        lambtha is the L2 regularization parameter
+        prev (tensor): contains the output of the previous layer.
+        n (n):  number of nodes the new layer should contain.
+        activation (tensor): the activation function that should be used
+                             on the layer.
+        lambtha (float): the L2 regularization parameter.
+    Returns:
+        tensor: the output of the new layer.
     """
-    mode = "FAN_AVG"
-    regulizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode=mode)
-    layer = tf.layers.Dense(kernel_initializer=initializer,
-                            kernel_regularizer=regulizer,
-                            units=n,
-                            activation=activation)
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n,
+                            activation=activation,
+                            kernel_initializer=init,
+                            kernel_regularizer=reg)
     return layer(prev)
