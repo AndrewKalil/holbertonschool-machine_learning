99a6f68bce33da25645efe0853dbe614ff7de77d
AndrewKalil <1541@holbertonschool.com>
2021-01-20 15:55:49 -0500

    fixing 4

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -16,23 +16,30 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         keep_prob is the probability that a node will be kept
     """
 
-    cache = dict()
+    cache = {}
     cache['A0'] = X
 
-    for layer in range(L):
-        current_W = weights['W' + str(layer+1)]
-        current_b = weights['b' + str(layer+1)]
-        A_prev = cache['A' + str(layer)]
-        z = (np.matmul(current_W, A_prev)) + current_b
-        dropout = np.random.binomial(1, keep_prob, size=z.shape)
+    for la in range(1, L + 1):
+        keyA = "A{}".format(la)
+        keyA_p = "A{}".format(la - 1)
+        keyD = "D{}".format(la)
+        keyW = "W{}".format(la)
+        keyb = "b{}".format(la)
 
-        if layer == L:
-            t = np.exp(z)
-            cache['A' + str(layer+1)] = t / np.sum(t, axis=0, keepdims=True)
+        Z = np.matmul(weights[keyW], cache[keyA_p]) + weights[keyb]
+
+        if la != L:
+            A = (np.exp(Z) - np.exp(-Z)) / (np.exp(Z) + np.exp(-Z))
+            D = np.random.rand(A.shape[0], A.shape[1])
+            D = np.where(D < keep_prob, 1, 0)
+            cache[keyD] = D
+            A *= D
+            A /= keep_prob
+            cache[keyA] = A
         else:
-            cache['A' + str(layer+1)] = np.tanh(z)
-            cache['D' + str(layer+1)] = dropout
-            cache['A' + str(layer+1)] *= dropout
-            cache['A' + str(layer+1)] /= keep_prob
+            # Softmax
+            t = np.exp(Z)
+            A = t / np.sum(t, axis=0, keepdims=True)
+            cache[keyA] = A
 
     return cache
