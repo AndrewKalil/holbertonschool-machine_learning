967f92d9e31e1264276688ec6973e89ad444c3c4
AndrewKalil <1541@holbertonschool.com>
2021-01-26 10:17:21 -0500

    pep8 fix

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -31,8 +31,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(units=layers[i],
-                                       activation=activations[i],
-                                       kernel_regularizer=reg,
-                                       ))
+                                 activation=activations[i],
+                                 kernel_regularizer=reg,
+                                 ))
 
     return model
