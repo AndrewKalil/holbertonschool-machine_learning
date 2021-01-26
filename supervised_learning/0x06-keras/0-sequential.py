8d8136dbd591872276f7d4f2a348adaca8fe47d8
AndrewKalil <1541@holbertonschool.com>
2021-01-26 10:14:43 -0500

    adding files

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Keras """
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """builds a neural network with the Keras library
+
+    Args:
+        nx is the number of input features to the network
+        layers is a list containing the number of nodes in each layer of
+          the network
+        activations is a list containing the activation functions used for
+          each layer of the network
+        lambtha is the L2 regularization parameter
+        keep_prob is the probability that a node will be kept for dropout
+    """
+    # initialize sequential model
+    model = K.Sequential()
+
+    # adding regulations
+    reg = K.regularizers.L1L2(l2=lambtha)
+
+    # first densly connected layer
+    model.add(K.layers.Dense(units=layers[0],
+                             activation=activations[0],
+                             kernel_regularizer=reg,
+                             input_shape=(nx,),))
+
+    # subsequent densely connected layers:
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(units=layers[i],
+                                       activation=activations[i],
+                                       kernel_regularizer=reg,
+                                       ))
+
+    return model
