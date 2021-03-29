46568804b98c3a7128d9231ce58cb6610a2d1253
AndrewKalil <1541@holbertonschool.com>
2021-03-29 18:25:42 -0500

    adding up to 4

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+""" Clustering """
+
+import numpy as np
+
+
+def variance(X, C):
+    """ calculates the total intra-cluster variance for a data set.
+            Args:
+                    X: (numpy.ndarray) containing the data set.
+                    C: (numpy.ndarray) containing the centroid
+                        means for each cluster.
+            Returns:
+                    (float) the total variance.
+    """
+    try:
+        if X.shape[1] != C.shape[1]:
+            return None
+        distance = np.sqrt((X[:, np.newaxis, 0] - C[:, 0])**2 +
+                           (X[:, np.newaxis, 1] - C[:, 1])**2)
+        min_dist = np.min(distance, axis=1)
+        return np.sum(min_dist ** 2)
+    except Exception as e:
+        return None
