1cac20eed57e56292547177326ea897f62bf7139
AndrewKalil <1541@holbertonschool.com>
2021-05-20 21:40:02 -0500

    changes to task 5

--- supervised_learning/0x11-attention/5-sdp_attention.py
+++ supervised_learning/0x11-attention/5-sdp_attention.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python3
-""" Module used to """
+"""
+Function that calcuylates the scaled dot product attention
+"""
 
 
 import tensorflow as tf
 
 
 def sdp_attention(Q, K, V, mask=None):
-    """[calculates the scaled dot product attention:]
-    Args:
-        Q ([tensor]):       [is a tensor with its last two dimensions as
-                            (..., seq_len_q, dk) containing the query matrix]
-        K ([tensor]):       [is a tensor with its last two dimensions as
-                            (..., seq_len_q, dk)  containing the key matrix]
-        V ([tensor]):       [is a tensor with its last two dimensions as
-                            (..., seq_len_q, dk) containing the value matrix]
-        mask ([tensor], optional): [tensor that can be broadcast into
-                                    (..., seq_len_q, seq_len_v) containing
-                                    the optional mask]. Defaults to None.
-        if mask is not None, multiply -1e9 to the mask and add it to the
-        scaled matrix multiplication
-    The preceding dimensions of Q, K, and V are the same
-    Returns: output, weights
-        outputa [tensor]: [tensor with its last two dimensions as
-                           (..., seq_len_q, dv) containing the scaled dot
-                           product attention]
-        weights [tensor]: [tensor with its last two dimensions as
-                          (..., seq_len_q, seq_len_v) containing the
-                          attention weights
+    """
+    calculates scaled dot product attention
     """
 
-    # scale matmul_qk with scaled_attention_logits
     matmul_qk = tf.matmul(Q, K, transpose_b=True)
-    sal = matmul_qk / tf.math.sqrt(tf.cast(tf.shape(K)[-1], tf.float32))
+    scald_qk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scald_atten = matmul_qk / tf.math.sqrt(scald_qk)
 
-    # add the mask
-    sal = sal + (mask * -1e9) if mask is not None else sal
-
-    # softmax
-    weights = tf.nn.softmax(sal, axis=-1)
+    if mask is not None:
+        scald_atten += (mask * -1e9)
+    weights = tf.nn.softmax(scald_atten, axis=-1)
     output = tf.matmul(weights, V)
 
     return output, weights
